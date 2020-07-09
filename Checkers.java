925c8f8bb8035031394caec0acd5cbca647ac2cd
mishtak00 <33734216+mishtak00@users.noreply.github.com>
2020-07-09 22:24:10 +0200

    Add files via upload

--- Checkers.java
+++ Checkers.java
@@ -1,7 +1,4 @@
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Checkers {
 
@@ -45,19 +42,60 @@ public class Checkers {
             for (int j=0; j<boardSize; j++)
                 board[i][j]=Square.EMPTY;
         board[5][0]=Square.SELF; board[4][1]=Square.OPPON;
+        board[2][1]=Square.OPPONKING; board[0][1]=Square.OPPON;
 
         System.out.println(this);
-//        printBoardSquare(new int[]{7,7});
+
         List<int[]> dList = getDiagSquares(5,0);
         for (int[] sq : dList)
             printBoardSquare(sq);
 
         System.out.println(isOther(5,0,4,1));
         System.out.println(canCapture(5,0,4,1));
+        List<int[]> mList = getMovesRecur(5,0);
+        for (int[] mv : mList)
+            printBoardSquare(mv);
+
+        board[5][2]=Square.SELFKING;
+        System.out.println(this);
+        List<int[]> dList2 = getDiagSquares(5,2);
+        for (int[] sq : dList2)
+            printBoardSquare(sq);
+
+        List<int[]> mList2 = getMovesRecur(5,2);
+        for (int[] mv : mList2)
+            printBoardSquare(mv);
+
+        board[4][3]=Square.OPPONKING;
+        System.out.println(this);
+        Map<int[],List<int[]>> mvSet = getMovesSelf();
+        for (Map.Entry<int[], List<int[]>> mvs : mvSet.entrySet()) {
+            System.out.printf("Moves for %s : %s\n",
+                    boardSquareToString(mvs.getKey()),
+                    moveListToString(mvs.getValue()));
+        }
+
+    }
+
+    private String boardSquareToString(int[] s) {
+        return String.format("(%c,%d)", (char)('A'+s[1]), boardSize-s[0]);
     }
 
     private void printBoardSquare(int[] s) {
-        System.out.printf("(%c,%d)\n", (char)('A'+s[1]), boardSize-s[0]);
+        System.out.println(boardSquareToString(s));
+    }
+
+    private String moveListToString(List<int[]> ml) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{ ");
+        for (int[] mv : ml)
+            sb.append(String.format("%s ", boardSquareToString(mv)));
+        sb.append("}");
+        return sb.toString();
+    }
+
+    private void printMoveList(List<int[]> ml) {
+        System.out.println(moveListToString(ml));
     }
 
     private static String squareToString(Square sq) {
@@ -100,19 +138,42 @@ public class Checkers {
         return sb.toString();
     }
 
+    /*
+     * Gets diagonal squares of the square at (i,j) depending
+     * on its type. This expects a non-EMPTY square.
+     * This function is the only one that knows to constrain
+     * the normal pieces' moves as opposed to the kings'.
+     */
     private List<int[]> getDiagSquares(int i, int j) {
         List<int[]> dList = new LinkedList<>();
-        if (i+1<boardSize) {
-            if (j+1<boardSize)
-                dList.add(new int[]{i + 1, j + 1});
-            if (j-1>=0)
-                dList.add(new int[]{i + 1, j - 1});
-        }
-        if (i-1>=0) {
-            if (j+1<boardSize)
-                dList.add(new int[]{i - 1, j + 1});
-            if (j-1>=0)
-                dList.add(new int[]{i - 1, j - 1});
+        Square curr = board[i][j];
+
+        // only forward diagonals for normals
+        if (curr==Square.SELF || curr==Square.OPPON) {
+            // find diags: towards lower indices for self
+            // towards higher indices for opponent
+            int selfOrOpp = board[i][j]==Square.SELF ? -1 : 1;
+            if (i+selfOrOpp<boardSize) {
+                if (j+1<boardSize)
+                    dList.add(new int[]{i+selfOrOpp, j+1});
+                if (j-1>=0)
+                    dList.add(new int[]{i+selfOrOpp, j-1});
+            }
+
+        // both forward and backward diagonals for kings
+        } else if (curr==Square.SELFKING || curr==Square.OPPONKING) {
+            if (i-1>=0) {
+                if (j+1<boardSize)
+                    dList.add(new int[]{i-1, j+1});
+                if (j-1>=0)
+                    dList.add(new int[]{i-1, j-1});
+            }
+            if (i+1<boardSize) {
+                if (j-1>=0)
+                    dList.add(new int[]{i+1, j-1});
+                if (j+1<boardSize)
+                    dList.add(new int[]{i+1, j+1});
+            }
         }
         return dList;
     }
@@ -122,11 +183,9 @@ public class Checkers {
         return (
                 // can move to other square
                 // if am self and other is oppon
-                ((s1==Square.SELF || s1==Square.SELFKING)
-                    && (s2==Square.OPPON || s2==Square.OPPONKING))
+                ((isSelf(s1)) && (isOppon(s2)))
                 // or if am oppon and other is self
-                || ((s1==Square.OPPON || s1==Square.OPPONKING)
-                    && (s2==Square.SELF || s2==Square.SELFKING))
+                || ((isOppon(s1)) && (isSelf(s2)))
         );
     }
 
@@ -140,7 +199,7 @@ public class Checkers {
                 && destx>=0 && destx<boardSize
                 && desty>=0 && desty<boardSize
                 // and if it is empty
-                && board[destx][desty]==Square.EMPTY
+                && isEmpty(board[destx][desty])
                 );
     }
 
@@ -148,6 +207,7 @@ public class Checkers {
         List<int[]> move = new LinkedList<>();
 
         for (int[] diag : getDiagSquares(i,j)) {
+
             // move over and capture opponent square
             if (canCapture(i, j, diag[0], diag[1])) {
                 int destx = i+2*(diag[0]-i), desty = j+2*(diag[1]-j);
@@ -164,17 +224,31 @@ public class Checkers {
             }
 
             // move to empty square
+
         }
 
         return move;
     }
 
-    private Set<int[][]> getMovesSelf() {
-        Set<int[][]> moves = new HashSet<>();
+    private static boolean isSelf(Square s) {
+        return s==Square.SELF || s==Square.SELFKING;
+    }
+
+    private static boolean isOppon(Square s) {
+        return s==Square.OPPON || s==Square.OPPONKING;
+    }
+
+    private static boolean isEmpty(Square s) {
+        return s==Square.EMPTY;
+    }
+
+    private Map<int[], List<int[]>> getMovesSelf() {
+        Map<int[], List<int[]>> moves = new HashMap<>();
         for (int i=boardSize-1; i>=0; i--)
             for (int j=0; j<boardSize; j++)
-                ;
-
+                if (isSelf(board[i][j])) {
+                    moves.put(new int[]{i,j},getMovesRecur(i,j));
+                }
         return moves;
     }
 
